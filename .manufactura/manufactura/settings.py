9fb3c17cbf9c8ec649174e04a6d3206d749bcdeb
dmytrolarin <dmytr.larin@gmail.com>
2022-06-08 21:54:29 +0200

    Привязана новая ел. почта

--- .manufactura/manufactura/settings.py
+++ .manufactura/manufactura/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'manufactura.restoraunt@gmail.com'
-EMAIL_HOST_PASSWORD = 'objhwpmvjutjqgqv'
+EMAIL_HOST_USER = 'manufactura.restaurant@gmail.com'
+EMAIL_HOST_PASSWORD = 'sdjqwohgpiznvujm'
