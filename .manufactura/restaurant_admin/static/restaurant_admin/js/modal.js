d3d054c3a247e10a478de43feeece140b10424e6
Стопычев Игорь <stopkin241@gmail.com>
2022-07-13 12:33:05 +0300

    Добавлено модальное окно при попытке отменить заказ

--- .manufactura/restaurant_admin/static/restaurant_admin/js/modal.js
+++ .manufactura/restaurant_admin/static/restaurant_admin/js/modal.js
@@ -1,26 +1,25 @@
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal__content');
-const closer = document.querySelector('.modal__close');
-const wrapperList = document.querySelectorAll('.wrapper-main');
-const title = document.querySelector('.detail__title');
-const composition = document.querySelector('.detail__description');
-const cost = document.querySelector('.detail__price');
-const button  = document.querySelector('.detail__bagBtn');
-wrapperList.forEach((list, index) => {
-    const view = list.querySelector(".cancel");
+const closer = document.querySelector('.no');
+const productList = document.querySelectorAll('.order');
+const title = document.querySelector(".detail__title")
+
+productList.forEach((list, index) => {
+    const view = list.querySelector('.cancel');
+    const name_not_main = list.querySelectorAll(".name");
+    const name_main = Array.from(name_not_main).map(el => el.textContent);
     view.addEventListener('click', () => {
         modal.classList.add('modal--bg');
         modalContent.classList.add('modal__content--show');
-        title.innerText = "name_main";
-        composition.innerText = "composition_main";
-        cost.innerText = "price_main";
+        title.innerText = name_main;
     });
 });
-  
+
 closer.addEventListener('click', () => {
     modal.classList.remove('modal--bg');
     modalContent.classList.remove('modal__content--show');
 });
+
 
 modal.addEventListener('click', () => {
     modal.classList.remove('modal--bg');
